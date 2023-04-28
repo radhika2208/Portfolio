073b9e86238dc0e7be322862aa56fedb7cfc033a
radhika2208 <69978298+radhika2208@users.noreply.github.com>
2023-04-28 09:52:46 +0530

    Updated

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -129,4 +129,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'radhikapiplani12@gmail.com'
-EMAIL_HOST_PASSWORD = 'ovpwlhywhlrgnnzm'
+EMAIL_HOST_PASSWORD = ''
